68bdd2cbc5ebfcf9a9424fe400587233543034d7
Richard Meats <35453720+rjmeats@users.noreply.github.com>
2018-01-17 17:31:07 +0000

    Updated HWHelper2 on branch1

--- HelloWorld/HWHelper2.java
+++ HelloWorld/HWHelper2.java
@@ -2,7 +2,9 @@
 public class HWHelper2 {
 
 	static String getHelperString() {
-		return "Helper2 says hello - branch1 version" + "\r\n" + 
+		String s = "Helper2 says hello - branch1 version" + "\r\n" + 
 				HWBranch1Helper.getHelperString();
+		s += "\r\n" + "change made to branch1";
+		return s;
 	}
 }
