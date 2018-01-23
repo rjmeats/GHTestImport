5d6e6ea05bc81466bb87def7eba5659c9fb41ad8
Richard Meats <35453720+rjmeats@users.noreply.github.com>
2018-01-23 09:51:54 +0000

    Add to end of HWMain

--- HelloWorld/HWMain.java
+++ HelloWorld/HWMain.java
@@ -9,6 +9,8 @@ public class HWMain {
 		System.out.println("Hello again");
 		System.out.println("");
 		System.out.println(HWHelper2.getHelperString());
+		System.out.println("");
+		System.out.println("Added at the end of main");
 	}
 	// Comment added in branch1
 }
